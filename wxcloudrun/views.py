5513ec712a7e7309661ac58b47e97841ed9ec0cf
zhengyu <zhengyu@163.com>
2024-07-05 10:54:12 +0800

    测试文本提交

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -64,3 +64,11 @@ def get_count():
     """
     counter = Counters.query.filter(Counters.id == 1).first()
     return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+
+
+@app.route('/test')
+def index():
+    """
+    :return: 返回index页面
+    """
+    return "测试文本"
