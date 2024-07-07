c672a9d97cc0fbce3973f1934e0a65ecfbfd319f
welcomemonth <midrain1999@163.com>
2024-07-07 22:20:17 +0800

    后台检测

--- run.py
+++ run.py
@@ -10,6 +10,15 @@ app.add_url_rule(rule='/robot/', # WeRoBot 挂载地址
                  view_func=make_view(myrobot),
                  methods=['GET', 'POST'])
 
+# 处理微信后台检测请求
+@app.route('/check', methods=['POST', 'GET'])
+def check():
+    data = request.get_json()
+    if data and data.get("action") == "CheckContainerPath":
+        return "success", 200
+    return "", 200
+
+
 # 启动Flask Web服务
 if __name__ == '__main__':
     app.run(host=sys.argv[1], port=sys.argv[2])
