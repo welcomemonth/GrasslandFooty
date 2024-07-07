9ae020428cd9cee733a4c679da4bd54f4823de63
welcomemonth <midrain1999@163.com>
2024-07-07 22:41:31 +0800

    merge

--- run.py
+++ run.py
@@ -5,21 +5,21 @@ from flask import Flask, request
 from werobot.contrib.flask import make_view
 
 app = Flask(__name__)
-app.add_url_rule(rule='/robot/', # WeRoBot 挂载地址
-                 endpoint='werobot', # Flask 的 endpoint
-                 view_func=make_view(myrobot),
-                 methods=['GET', 'POST'])
 
-# 处理微信后台检测请求
-@app.route('/check', methods=['POST', 'GET'])
-def check():
-    data = request.get_json()
-    if data and data.get("action") == "CheckContainerPath":
-        return "success", 200
-    return "", 200
+# WeRoBot 挂载地址和处理微信后台检测请求
+@app.route('/', methods=['GET', 'POST'])
+def combined_view():
+    # 检查微信后台检测请求
+    if request.method == 'POST' and request.is_json:
+        data = request.get_json()
+        if data and data.get("action") == "CheckContainerPath":
+            return "success", 200
+
+    # 处理 WeRoBot 请求
+    view_func = make_view(myrobot)
+    return view_func()
 
 
-# 启动Flask Web服务
+# 启动 Flask Web 服务
 if __name__ == '__main__':
-    app.run(host=sys.argv[1], port=sys.argv[2])
-
+    app.run(host=sys.argv[1], port=int(sys.argv[2]))
